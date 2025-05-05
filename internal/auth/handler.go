99d20496a51aa66b27f8ae63264e00135d19b036
jamal23041989 <jamalkurbanov23041989@gmail.com>
2025-05-05 23:35:48 +0300

    refactoring handler and payload

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,8 +1,8 @@
 package auth
 
 import (
-	"fmt"
 	"github.com/jamal23041989/go_short_links/configs"
+	"github.com/jamal23041989/go_short_links/pkg/req"
 	"github.com/jamal23041989/go_short_links/pkg/resp"
 	"net/http"
 )
@@ -17,6 +17,11 @@ type AuthHandler struct {
 
 func (h *AuthHandler) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		_, err := req.HandleBody[LoginRequest](&w, r)
+		if err != nil {
+			return
+		}
+
 		data := LoginResponse{
 			Token: h.Config.Auth.Secret,
 		}
@@ -26,7 +31,15 @@ func (h *AuthHandler) Login() http.HandlerFunc {
 
 func (h *AuthHandler) Register() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Registration")
+		_, err := req.HandleBody[RegisterRequest](&w, r)
+		if err != nil {
+			return
+		}
+
+		data := RegisterResponse{
+			Token: h.Config.Auth.Secret,
+		}
+		resp.Json(w, data, 200)
 	}
 }
 
