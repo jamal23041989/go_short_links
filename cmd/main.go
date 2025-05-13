0557bbd7d713ecc24ca29205efbca3d8734f5ead
jamal23041989 <jamalkurbanov23041989@gmail.com>
2025-05-14 01:33:01 +0300

    Refactoring main.go

--- cmd/main.go
+++ cmd/main.go
@@ -5,8 +5,10 @@ import (
 	"github.com/jamal23041989/go_short_links/configs"
 	"github.com/jamal23041989/go_short_links/internal/auth"
 	"github.com/jamal23041989/go_short_links/internal/link"
+	"github.com/jamal23041989/go_short_links/internal/stat"
 	"github.com/jamal23041989/go_short_links/internal/user"
 	"github.com/jamal23041989/go_short_links/pkg/db"
+	"github.com/jamal23041989/go_short_links/pkg/event"
 	"github.com/jamal23041989/go_short_links/pkg/middleware"
 	"net/http"
 )
@@ -15,11 +17,17 @@ func main() {
 	conf := configs.LoadConfig()
 	dbNew := db.NewDb(conf)
 	router := http.NewServeMux()
+	eventBus := event.NewEventBus()
 
 	linkRepository := link.NewLinkRepository(dbNew)
 	userRepository := user.NewUserRepository(dbNew)
+	statRepository := stat.NewStatRepository(dbNew)
 
 	authService := auth.NewAuthService(userRepository)
+	statService := stat.NewStatService(&stat.StatServiceDeps{
+		EventBus:       eventBus,
+		StatRepository: statRepository,
+	})
 
 	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
 		Config:      conf,
@@ -27,6 +35,11 @@ func main() {
 	})
 	link.NewLinkHandler(router, link.LinkHandlerDeps{
 		LinkRepository: linkRepository,
+		Config:         conf,
+		EventBus:       eventBus,
+	})
+	stat.NewStatHandler(router, stat.StatHandlerDeps{
+		StatRepository: statRepository,
 		Config:         conf,
 	})
 
@@ -39,6 +52,8 @@ func main() {
 		Addr:    ":8081",
 		Handler: stack(router),
 	}
+
+	go statService.AddClick()
 
 	fmt.Println("Server is listening on port 8081")
 	server.ListenAndServe()
